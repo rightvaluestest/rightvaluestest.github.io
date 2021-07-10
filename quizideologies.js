8527351e575af432d90c53e627e00def1f6511e7
rightvaluestest <61039650+rightvaluestest@users.noreply.github.com>
2021-07-10 12:41:48 +0800

    Update quizideologies.js

--- quizideologies.js
+++ quizideologies.js
@@ -1,251 +1,251 @@
 const ideologies = {
-    
-    "Fascism": {
-            "a": 10,
-            "b": 85,
-            "c": 90,
-            "d": 10,
-            "e": 0,
-            "f": 50,
-            "g": 80
+    'Fascism': {
+            a: 20,
+            b: 85,
+            c: 85,
+            d: 10,
+            e: 0,
+            f: 50,
+            g: 80
     },
     
-    "Nazism": {
-            "b": 90,
-            "c": 95,
-            "d": 5,
-            "e": 5,
-            "f": 50,
-            "g": 100
+    'Nazism': {
+            a: 0
+            b: 80,
+            c: 100,
+            d: 5,
+            e: 5,
+            f: 50,
+            g: 100
     },
    
-    "Absolute Monarchism": {
-            "a": 30,
-            "b": 50,
-            "c": 80,
-            "d": 80,
-            "e": 80,
-            "f": 30,
-            "g": 70
+    'Absolute Monarchism': {
+            a: 30,
+            b: 50,
+            c: 80,
+            d: 80,
+            e: 80,
+            f: 30,
+            g: 70
     },
     
-    "Feudalism": {
-            "a": 30,
-            "b": 20,
-            "c": 70,
-            "d": 80,
-            "e": 80,
-            "f": 50,
-            "g": 70
+    'Feudalism': {
+            a: 30,
+            b: 20,
+            c: 70,
+            d: 80,
+            e: 80,
+            f: 50,
+            g: 70
     },
     
-    "Constitutional Monarchism": {
-            "a": 20,
-            "b": 30,
-            "c": 60,
-            "d": 40,
-            "e": 80,
-            "f": 50,
-            "g": 70
+    'Constitutional Monarchism': {
+            a: 20,
+            b: 30,
+            c: 60,
+            d: 40,
+            e: 80,
+            f: 50,
+            g: 70
     },
    
-     "Conservatism": {
-            "a": 10,
-            "b": 20,
-            "c": 40,
-            "d": 20,
-            "e": 70,
-            "f": 50,
-            "g": 10
+     'Conservatism': {
+            a: 10,
+            b: 20,
+            c: 40,
+            d: 20,
+            e: 70,
+            f: 50,
+            g: 10
     },
     
-    "Neo-Conservatism": {
-            "a": 10,
-            "b": 20,
-            "c": 40,
-            "d": 20,
-            "e": 60,
-            "f": 60,
-            "g": 10
+    'Neo-Conservatism': {
+            a: 10,
+            b: 20,
+            c: 40,
+            d: 20,
+            e: 60,
+            f: 60,
+            g: 10
     },
     
-    "Libertarianism": {
-            "a": 60,
-            "b": 10,
-            "c": 20,
-            "d": 60,
-            "e": 50,
-            "f": 80,
-            "g": 20
+    'Libertarianism': {
+            a: 60,
+            b: 10,
+            c: 20,
+            d: 60,
+            e: 50,
+            f: 80,
+            g: 20
     },
     
-    "Individualist Anarchism": {
-            "a": 50,
-            "b": 10,
-            "c": 10,
-            "d": 80,
-            "e": 50,
-            "f": 60,
-            "g": 10
+    'Individualist Anarchism': {
+            a: 50,
+            b: 10,
+            c: 10,
+            d: 80,
+            e: 50,
+            f: 60,
+            g: 10
     },
     
-    "Agorism" : {
-            "a": 80,
-            "b": 10,
-            "c": 10,
-            "d": 80,
-            "e": 30,
-            "f": 50,
-            "g": 10
+    'Agorism' : {
+            a: 80,
+            b: 10,
+            c: 10,
+            d: 80,
+            e: 30,
+            f: 50,
+            g: 10
     },
 
-    "Anarcho-Capitalism": {
-            "a": 50,
-            "b": 0,
-            "c": 10,
-            "d": 50,
-            "e": 20,
-            "f": 80,
-            "g": 50
+    'Anarcho-Capitalism': {
+            a: 50,
+            b: 0,
+            c: 10,
+            d: 50,
+            e: 20,
+            f: 80,
+            g: 50
     },
     
-    "Minarchism": {
-            "a": 50,
-            "b": 30,
-            "c": 15,
-            "d": 50,
-            "e": 50,
-            "f": 30,
-            "g": 20
+    'Minarchism': {
+            a: 50,
+            b: 30,
+            c: 15,
+            d: 50,
+            e: 50,
+            f: 30,
+            g: 20
     },
     
-    "Technocracy": {
-            "a": 80,
-            "b": 60,
-            "c": 70,
-            "d": 60,
-            "e": 20,
-            "f": 80,
-            "g": 20
+    'Technocracy': {
+            a: 80,
+            b: 60,
+            c: 70,
+            d: 60,
+            e: 20,
+            f: 80,
+            g: 20
     },
 
-    "Neo-Liberalism": {
-            "a": 70,
-            "b": 30,
-            "c": 30,
-            "d": 50,
-            "e": 30,
-            "f": 70,
-            "g": 20
+    'Neo-Liberalism': {
+            a: 70,
+            b: 30,
+            c: 30,
+            d: 50,
+            e: 30,
+            f: 70,
+            g: 20
     },
     
-    "Paleolibertarianism": {
-            "a": 30,
-            "b": 30,
-            "c": 20,
-            "d": 40,
-            "e": 30,
-            "f": 60,
-            "g": 20
+    'Paleolibertarianism': {
+            a: 30,
+            b: 30,
+            c: 20,
+            d: 40,
+            e: 30,
+            f: 60,
+            g: 20
     },
     
-    "Geo-Libertarianism": {
-            "a": 60,
-            "b": 55,
-            "c": 20,
-            "d": 40,
-            "e": 50,
-            "f": 60,
-            "g": 20
+    'Geo-Libertarianism': {
+            a: 60,
+            b: 55,
+            c: 20,
+            d: 40,
+            e: 50,
+            f: 60,
+            g: 20
     },
     
-    "Centre-Right Politics": {
-            "a": 60,
-            "b": 50,
-            "c": 50,
-            "d": 50,
-            "e": 50,
-            "f": 50,
-            "g": 40
+    'Centre-Right Politics': {
+            a: 60,
+            b: 50,
+            c: 50,
+            d: 50,
+            e: 50,
+            f: 50,
+            g: 40
     },
 
-    "name": "Theocracy": {
-            "a": 50,
-            "b": 50,
-            "c": 80,
-            "d": 50,
-            "e": 90,
-            "f": 40,
-            "g": 20
+    'Theocracy': {
+            a: 50,
+            b: 50,
+            c: 80,
+            d: 50,
+            e: 90,
+            f: 40,
+            g: 20
    },
    
-   "name": "Reactionary Conservatism": {
-            "a": 10,
-            "b": 20,
-            "c": 80,
-            "d": 70,
-            "e": 80,
-            "f": 50,
-            "g": 10
+   'Reactionary Conservatism': {
+            a: 10,
+            b: 20,
+            c: 80,
+            d: 70,
+            e: 80,
+            f: 50,
+            g: 10
     },
 
-    "Pink Capitalism": {
-            "a": 80,
-            "b": 10,
-            "c": 10,
-            "d": 30,
-            "e": 50,
-            "f": 70,
-            "g": 20
+    'Pink Capitalism': {
+            a: 80,
+            b: 10,
+            c: 10,
+            d: 30,
+            e: 50,
+            f: 70,
+            g: 20
     },
    
-    "Paleoconservatism": {
-            "a": 20,
-            "b": 40,
-            "c": 50,
-            "d": 60,
-            "e": 60,
-            "f": 40,
-            "g": 20
+    'Paleoconservatism': {
+            a: 20,
+            b: 40,
+            c: 50,
+            d: 60,
+            e: 60,
+            f: 40,
+            g: 20
     },
     
-    "Right-Wing Populism": {
-            "a": 30,
-            "b": 30,
-            "c": 40,
-            "d": 70,
-            "e": 55,
-            "f": 50,
-            "g": 50
+    'Right-Wing Populism': {
+            a: 30,
+            b: 30,
+            c: 40,
+            d: 70,
+            e: 55,
+            f: 50,
+            g: 50
     },
 
-   "Civic Nationalism": {
-            "a": 60,
-            "b": 30,
-            "c": 35,
-            "d": 50,
-            "e": 55,
-            "f": 50,
-            "g": 50
+   'Civic Nationalism': {
+            a: 60,
+            b: 30,
+            c: 35,
+            d: 50,
+            e: 55,
+            f: 50,
+            g: 50
     },
     
-    "Progressive Conservatism": {
-            "a": 60,
-            "b": 30,
-            "c": 35,
-            "d": 50,
-            "e": 55,
-            "f": 50,
-            "g": 50
+    'Progressive Conservatism': {
+            a: 60,
+            b: 30,
+            c: 35,
+            d: 50,
+            e: 55,
+            f: 50,
+            g: 50
     },
     
-    "Classical Liberalism": {
-            "a": 40,
-            "b": 30,
-            "c": 40,
-            "d": 40,
-            "e": 60,
-            "f": 50,
-            "g": 20
+    'Classical Liberalism': {
+            a: 40,
+            b: 30,
+            c: 40,
+            d: 40,
+            e: 60,
+            f: 50,
+            g: 20
     },
 }
