4cb7ebaf5fd1e9da264d174c505c62213289b11c
rightvaluestest <61039650+rightvaluestest@users.noreply.github.com>
2021-07-20 22:04:21 +0800

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -2,45 +2,54 @@ ideologies = [
 { "name": "<a href='https://en.wikipedia.org/wiki/Fascism'> Marxism-Leninism </a>",
   "desc": "Fascism is a form of authoritarian ultranationalism characterized by dictatorial power, forcible suppression of opposition, and strong regimentation of society and of the economy, which came to prominence in early 20th-century Europe.",
   "i18n": { 
-    "ko": { "name": "마르크스-레닌주의", 
-            "desc": "마르크스-레닌주의는 소비에트 연방에서 20 세기에 형성된 마르크스주의의 한 형태입니다. 마르크스-레닌주의자들은 혁명을 달성하고 사회주의를 확립하기 위한 발판으로 공산당을 이용하는 것을 매우 선호합니다. 많은 마르크스주의-레닌주의자들은 다른 많은 마르크스주의자들보다 다소 국가주의적이고 애국심이 강하며, 환경보다 산업 발전을 선호할 수 있습니다." },
-    "ru": { "name": "Марксизм-ленинизм",
-            "desc": "Марксизм-ленинизм — это форма марксизма, которая была сформирована в 20 веке в Советском Союзе. Марксисты-ленинцы в значительной степени выступают за использование коммунистической политической партии в качестве платформы как для достижения революции, так и для утверждения социализма. Многие марксисты-ленинцы несколько более националистичны и патриотичны, чем многие другие марксисты, и могут отдавать предпочтение промышленному прогрессу перед экологическими целями." },
-    "cn": { "name": "马克思列宁主义", 
-            "desc": "马克思列宁主义（Marxism-Leninism，简称马列主义）是20世纪在苏联形成的马克思主义的一种形式。马列主义者非常赞成把共产主义政党作为实现革命和建立社会主义的平台。许多马列主义者比其他马克思主义者更具有民族主义和爱国主义色彩，而且可能更倾向于工业进步而不是环保目标。" },
-    "ja": { "name": "Marksizam-Lenjinizam", 
-            "desc": "Marksizam-lenjinizam je forma marksizma stvorena u XX vijeku u Sovjetskom Savezu. Marksisti-lenjinisti snažno podržavaju komunističke političke partije kao platforme za postizanje revolucije i osnivanje socijalizma. Mnogi marksisti-lenjinisti su donekle više nacionalistički od drugih marksista, i ponekad teže industrijskom napretku prije nego očuvanju prirodne sredine." }},
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
 "stats": { "a": 20, "b": 85, "c": 85, "d": 10, "e": 0, "f": 50, "g": 80 }},
-    
-    'Nazism': {
-            a: 0
-            b: 80,
-            c: 100,
-            d: 5,
-            e: 5,
-            f: 50,
-            g: 100
-    },
-   
-    'Absolute-Monarchism': {
-            a: 30,
-            b: 50,
-            c: 80,
-            d: 80,
-            e: 80,
-            f: 30,
-            g: 70
-    },
-    
-    'Constitutional-Monarchism': {
-            a: 20,
-            b: 30,
-            c: 60,
-            d: 40,
-            e: 80,
-            f: 50,
-            g: 70
-    },
+  
+ { "name": "<a href='https://en.wikipedia.org/wiki/Nazism'> National Socialism </a>",
+  "desc": "National Socialism, often shortened as Nazism, is a totalitarian ideology that has disdain for liberal democracy and the parliamentary system, and advocates for ideas like antisemitism, anti-communism, scientific racism, German nationalism, and eugenics.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 0, "b": 80, "c": 100, "d": 5, "e": 5, "f": 50, "g": 100 }},
+  
+ { "name": "<a href='https://en.wikipedia.org/wiki/Absolute_monarchy'> Absolute Monarchism </a>",
+  "desc": "Absolute Monarchism, also called Absolutism, is a form of monarchy in which the monarch holds supreme autocratic authority, principally not being restricted by written laws, legislature, or unwritten customs. These are often hereditary monarchies.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 30, "b": 50, "c": 80, "d": 80, "e": 80, "f": 30, "g": 50 }},
+  
+ { "name": "<a href='https://en.wikipedia.org/wiki/Constitutional_monarchy'> Constitutional Monarchism </a>",
+  "desc": "Constitutional Monarchism is a form of monarchy in which the monarch exercises authority in accordance with a written or unwritten constitution. Within Constitutional Monarchism, the monarch acts as a non-party political head of state under the constitution, whether written or unwritten.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 20, "b": 30, "c": 60, "d": 40, "e": 80, "f": 50, "g": 50 }},
    
      'Conservatism': {
             a: 10,
