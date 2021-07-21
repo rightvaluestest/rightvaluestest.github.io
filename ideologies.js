bd91f03335d7303433be2e073e26e0cc29573b9b
rightvaluestest <61039650+rightvaluestest@users.noreply.github.com>
2021-07-21 15:08:46 +0800

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,5 +1,7 @@
 ideologies = [
-{ "name": "<a href='https://en.wikipedia.org/wiki/Fascism'> Marxism-Leninism </a>",
+
+//Totalitarianism
+{ "name": "<a href='https://en.wikipedia.org/wiki/Fascism'> Fascism </a>",
   "desc": "Fascism is a form of authoritarian ultranationalism characterized by dictatorial power, forcible suppression of opposition, and strong regimentation of society and of the economy, which came to prominence in early 20th-century Europe.",
   "i18n": { 
     "ko": { "name": "", 
@@ -24,6 +26,9 @@ ideologies = [
     "ja": { "name": "", 
             "desc": "" }},
 "stats": { "a": 0, "b": 80, "c": 100, "d": 5, "e": 5, "f": 50, "g": 100 }},
+
+
+//Monarchism
   
  { "name": "<a href='https://en.wikipedia.org/wiki/Absolute_monarchy'> Absolute Monarchism </a>",
   "desc": "Absolute Monarchism, also called Absolutism, is a form of monarchy in which the monarch holds supreme autocratic authority, principally not being restricted by written laws, legislature, or unwritten customs. These are often hereditary monarchies.",
@@ -49,208 +54,277 @@ ideologies = [
             "desc": "" },
     "ja": { "name": "", 
             "desc": "" }},
-"stats": { "a": 20, "b": 30, "c": 60, "d": 40, "e": 80, "f": 50, "g": 50 }},
-   
-     'Conservatism': {
-            a: 10,
-            b: 20,
-            c: 40,
-            d: 20,
-            e: 70,
-            f: 50,
-            g: 10
-    },
-    
-    'Neoconservatism': {
-            a: 10,
-            b: 20,
-            c: 40,
-            d: 20,
-            e: 60,
-            f: 60,
-            g: 10
-    },
-    
-    'Libertarianism': {
-            a: 60,
-            b: 10,
-            c: 20,
-            d: 60,
-            e: 50,
-            f: 80,
-            g: 20
-    },
-    
-    'Individualist-Anarchism': {
-            a: 50,
-            b: 10,
-            c: 10,
-            d: 80,
-            e: 50,
-            f: 60,
-            g: 10
-    },
-    
-    'Agorism' : {
-            a: 80,
-            b: 10,
-            c: 10,
-            d: 80,
-            e: 30,
-            f: 50,
-            g: 10
-    },
+"stats": { "a": 30, "b": 30, "c": 60, "d": 40, "e": 80, "f": 50, "g": 50 }},
 
-    'Anarcho-Capitalism': {
-            a: 50,
-            b: 0,
-            c: 10,
-            d: 50,
-            e: 20,
-            f: 80,
-            g: 50
-    },
-    
-    'Minarchism': {
-            a: 50,
-            b: 30,
-            c: 15,
-            d: 50,
-            e: 50,
-            f: 30,
-            g: 20
-    },
-    
-    'Technocracy': {
-            a: 80,
-            b: 60,
-            c: 70,
-            d: 60,
-            e: 20,
-            f: 80,
-            g: 20
-    },
+{ "name": "<a href='https://en.wikipedia.org/wiki/Theocracy'> Theocracy </a>",
+  "desc": "Theocracy is a form of government in which a deity of some type is recognized as the supreme ruling authority, giving divine guidance to human intermediaries that manage the day-to-day affairs of the government. In some religions, the ruler, usually a king, was regarded as the chosen favorite of God (or gods) and could not be questioned, sometimes even being regarded as the descendant of or a god in their own right.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 20, "b": 50 "c": 80, "d": 50, "e": 90, "f": 40, "g": 50 }}, 
 
-    'Neoliberalism': {
-            a: 70,
-            b: 30,
-            c: 30,
-            d: 50,
-            e: 30,
-            f: 70,
-            g: 20
-    },
-    
-    'Paleolibertarianism': {
-            a: 30,
-            b: 30,
-            c: 20,
-            d: 40,
-            e: 30,
-            f: 60,
-            g: 20
-    },
-    
-    'Geo-Libertarianism': {
-            a: 60,
-            b: 60,
-            c: 20,
-            d: 40,
-            e: 50,
-            f: 60,
-            g: 20
-    },
-    
-    'Centre-Right-Politics': {
-            a: 60,
-            b: 50,
-            c: 50,
-            d: 50,
-            e: 50,
-            f: 50,
-            g: 40
-    },
 
-    'Theocracy': {
-            a: 50,
-            b: 50,
-            c: 80,
-            d: 50,
-            e: 90,
-            f: 40,
-            g: 20
-   },
-   
-   'Reactionary-Conservatism': {
-            a: 10,
-            b: 20,
-            c: 80,
-            d: 70,
-            e: 80,
-            f: 50,
-            g: 10
-    },
 
-    'Pink-Capitalism': {
-            a: 80,
-            b: 10,
-            c: 10,
-            d: 30,
-            e: 50,
-            f: 70,
-            g: 20
-    },
-   
-    'Paleoconservatism': {
-            a: 20,
-            b: 40,
-            c: 50,
-            d: 60,
-            e: 60,
-            f: 40,
-            g: 20
-    },
-    
-    'Right-Wing-Populism': {
-            a: 30,
-            b: 30,
-            c: 40,
-            d: 70,
-            e: 55,
-            f: 50,
-            g: 50
-    },
+//Liberals, Libertarians
 
-   'Civic-Nationalism': {
-            a: 60,
-            b: 30,
-            c: 35,
-            d: 50,
-            e: 55,
-            f: 50,
-            g: 50
-    },
+ { "name": "<a href='https://en.wikipedia.org/wiki/Right-Libertarianism'> Libertarianism </a>",
+  "desc": "Libertarianism, or more specifically Right-Libertarianism, is a political philosophy and movement that upholds liberty as a core principle. Libertarians seek to maximize autonomy and political freedom, emphasizing free association, freedom of choice, individualism and voluntary association.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 60, "b": 20, "c": 30, "d": 60, "e": 50, "f": 80, "g": 50 }},
+  
+ { "name": "<a href='https://en.wikipedia.org/wiki/Individualist-Anarchism'> Individualist-Anarchism </a>",
+  "desc": "Individualist anarchism is the branch of anarchism that emphasizes the individual and their will over external determinants such as groups, society, traditions and ideological systems.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 50, "b": 30, "c": 10, "d": 80, "e": 50, "f": 60, "g": 50 }},
     
-    'Progressive-Conservatism': {
-            a: 65,
-            b: 30,
-            c: 35,
-            d: 50,
-            e: 55,
-            f: 70,
-            g: 50
-    },
-    
-    'Classical-Liberalism': {
-            a: 40,
-            b: 30,
-            c: 40,
-            d: 40,
-            e: 60,
-            f: 50,
-            g: 20
-    },
-}
+ { "name": "<a href='https://en.wikipedia.org/wiki/Agorism'> Agorism </a>",
+  "desc": "Agorism is a free-market anarchist political philosophy that supports bringing about a society in which all relations between people are voluntary exchanges, and aims to achieve this through counter-economics.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 50, "b": 20, "c": 10, "d": 80, "e": 50, "f": 60, "g": 50 }},
 
+{ "name": "<a href='https://en.wikipedia.org/wiki/Anarcho-Capitalism'> Anarcho-Capitalism </a>",
+  "desc": "Anarcho-capitalism is a political philosophy and economic theory that advocates the elimination of centralized states in favor of a system of private property enforced by private agencies, free markets and the right-libertarian interpretation of self-ownership, which extends the concept to include control of private property as part of the self.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 50, "b": 10, "c": 10, "d": 50, "e": 20, "f": 80, "g": 50 }}, 
+
+{ "name": "<a href='https://en.wikipedia.org/wiki/Minarchism'> Minarchism </a>",
+  "desc": "Minarchism maintains that the state's only legitimate function is the protection of individuals from aggression, theft, breach of contract, and fraud, and the only legitimate governmental institutions are the military, police, and courts. Similarly to other Libertarian ideologies, it emphasizes on freedom of speech, property, and voluntary association.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 50, "b": 15, "c": 20, "d": 50, "e": 50, "f": 30, "g": 50 }}, 
+
+{ "name": "<a href='https://en.wikipedia.org/wiki/Paleolibertarianism'> Paleolibertarianism </a>",
+  "desc": "Paleolibertarianism is a variant of Libertarianism which stresses inherent incompatibility between progressivism and the concept of liberty, as well as a focus on the importance of inherited culture as a means of maintaining order. ",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 30, "b": 30 "c": 20, "d": 40, "e": 30, "f": 60, "g": 50 }}, 
+
+{ "name": "<a href='https://en.wikipedia.org/wiki/Geolibertarianism'>Geolibertarianism </a>",
+  "desc": "Geolibertarianism is a political and economic ideology that integrates libertarianism with Georgism, and holds that although people should own the value they produce themselves, all land should belong equally to all members of society. It does not consider land as legitimate private property.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 50, "b": 60 "c": 20, "d": 40, "e": 50, "f": 60, "g": 50 }},
+
+{ "name": "<a href='https://en.wikipedia.org/wiki/Neoliberalism'> Neoliberalism </a>",
+  "desc": "Neoliberalism is generally associated with policies of economic liberalization, including privatization, deregulation, globalization, free trade, and tricke-down economics, and served as the opposition of Keynesian Economics.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 60, "b": 30 "c": 50, "d": 50, "e": 30, "f": 70, "g": 50 }}, 
+
+{ "name": "<a href='https://en.wikipedia.org/wiki/Classical_Liberalism'> Classical Liberalism </a>",
+  "desc": "Classical liberalism is a political ideology and a branch of liberalism that advocates civil liberties under the rule of law with an emphasis on economic freedom. Highly influenced by the Enlightenment, it believes in a limited government that protects individual rights, and limited public services and welfare.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 40, "b": 30 "c": 40, "d": 40, "e": 60, "f": 50, "g": 50 }}, 
+
+
+//Traditionalism
+
+ { "name": "<a href='https://en.wikipedia.org/wiki/Conservatism'> Conservatism </a>",
+  "desc": "Conservatism is a cultural, social, and political philosophy which seeks to promote and to preserve traditional social institutions. n Western culture, conservatives seek to preserve a range of institutions such as organized religion, parliamentary government, and property rights.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 30, "b": 20, "c": 40, "d": 20, "e": 70, "f": 50, "g": 50 }},
+
+ { "name": "<a href='https://en.wikipedia.org/wiki/Neoconservatism'> Neoconservatism </a>",
+  "desc": "Neoconservatives typically advocate the promotion of democracy and interventionism in international affairs, including peace through strength, and are known for espousing disdain for communism and political radicalism.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 20, "b": 20, "c": 60, "d": 20, "e": 60, "f": 60, "g": 50 }},
+
+{ "name": "<a href='https://en.wikipedia.org/wiki/Centre-Right_Politics'> Centre-Right Politics </a>",
+  "desc": "Center-Right Politics lean to the right of the political spectrum, but are closer to the centre than others. It is influenced by economics liberalism and neoliberalism, but also supports certain left-leaning ideas such as a welfare state and egalitarianism.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 60, "b": 50 "c": 50, "d": 50, "e": 50, "f": 50, "g": 50 }}, 
+
+
+{ "name": "<a href='https://en.wikipedia.org/wiki/Reactionary'> Reactionary Conservatism </a>",
+  "desc": "Reactionary Conservatism, or Reactionaryism, favor a return to a previous political state of society that they believe possessed positive characteristics that are absent in contemporary society. In modern terms, Reactionary Conservatism refers to a highly traditional position that is opposed to social or political change, that tends to stand in opposition against Enlightenment ideas.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 10, "b": 20 "c": 80, "d": 70, "e": 80, "f": 50, "g": 50 }}, 
+
+{ "name": "<a href='https://en.wikipedia.org/wiki/Paleoconservatism'> Paleoconservatism </a>",
+  "desc": "Paleoconservatism is a political philosophy and variety of conservatism in the United States stressing nationalism, Christian ethics, regionalism, and traditionalist conservatism. Their views overlap with the Old Right and Right-Wing Populism.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 25, "b": 40 "c": 50, "d": 60, "e": 60, "f": 40, "g": 50 }}, 
+
+{ "name": "<a href='https://en.wikipedia.org/wiki/Right-Wing_Populism'> Right-Wing Populism </a>",
+  "desc": "Right-Wing Populism is a political ideology which combines right-wing politics and populist rhetoric and themes. The rhetoric often consists of anti-elitist and anti-intellectual sentiments, opposition to the Establishment, and speaking to the 'common people'.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 30, "b": 30 "c": 40, "d": 70, "e":55, "f": 50, "g": 50 }}, 
+
+
+//Others
+
+{ "name": "<a href='https://en.wikipedia.org/wiki/Rainbow_Capitalism'> Pink Capitalism </a>",
+  "desc": "Pink Capitalism is the incorporation of the LGBT movement and sexual diversity into consumerism and capitalism, in order to create a market that is oriented and tolerated towards LGBT communities.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 80, "b": 10 "c": 10, "d": 30, "e": 50, "f": 70, "g": 50 }}, 
+
+{ "name": "<a href='https://en.wikipedia.org/wiki/Civic_Nationalism'> Civic Nationalism </a>",
+  "desc": "Civic Nationalism is a form of nationalism identified by political philosophers who believe in an inclusive form of nationalism that adheres to traditional liberal values of freedom, tolerance, equality, and individual rights.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 60, "b": 30 "c": 50, "d": 50, "e": 55, "f": 50, "g": 50 }}, 
+
+{ "name": "<a href='https://en.wikipedia.org/wiki/Progressive_Conservatism'> Progressive Conservatism </a>",
+  "desc": "Civic Nationalism is a form of nationalism identified by political philosophers who believe in an inclusive form of nationalism that adheres to traditional liberal values of freedom, tolerance, equality, and individual rights.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 65, "b": 30 "c": 40, "d": 50, "e": 55, "f": 70, "g": 50 }}, 
+
+{ "name": "<a href='https://en.wikipedia.org/wiki/Technocracy'> Technocracy </a>",
+  "desc": "Technocracy is a system of government in which a decision-maker or makers are elected by the population or appointed on the basis of their expertise in a given area of responsibility, particularly with regard to scientific or technical knowledge.",
+  "i18n": { 
+    "ko": { "name": "", 
+            "desc": "" },
+    "ru": { "name": "",
+            "desc": "" },
+    "cn": { "name": "", 
+            "desc": "" },
+    "ja": { "name": "", 
+            "desc": "" }},
+"stats": { "a": 80, "b": 60 "c": 70, "d": 60, "e": 20, "f": 80, "g": 50 }}
 
 ];
