a205afbcdbb61cd38525bc872667556882abebc9
rightvaluestest <61039650+rightvaluestest@users.noreply.github.com>
2021-07-26 23:09:33 +0800

    Update i18n.js

--- i18n.js
+++ i18n.js
@@ -110,22 +110,22 @@ const i18n = {
      At the end of the quiz, your answers will be compared to the maximum possible for each value, thus giving you a percentage. Answer honestly!<br /><br /> There are <b><u>70</u></b> questions in the test. <br><br> \
      Similar to 8values, this quiz will attempt to match you with a specific rightist ideology, which is shown in the 'Closest Match' section. \
      Please remember that you are not intended to get a 100% score in any of the categories. The point of the quiz is to challenge your views. \
-     If you have any suggestions or constructive criticism please open an issue in the Github, linked in the upper right corner of this page. <br> <br> \   
+     If you have any suggestions or constructive criticism please open an issue in the Github, linked in the upper right corner of this page. <br> <br> \", 
     
      "ru": "Rightvalues это тест для людей правых взглядов, основанный на <a href='https://leftvalues.github.io/'>LeftValues</a>. Это викторина, которая пытается определить вашу позицию в спектре правых, как альтернатива LeftValues. Если вы не правый, эта викторина, очевидно, вам не подходит, и вам следует вместо этого пройти LeftValues. \
      Вам будет представлен тест, а затем вы ответите своим мнением по поводу утверждения, от <b>полностью согласен</b> до <b>полностью не согласен</b>, причем каждый ответ немного влияет на ваши результаты. В конце викторины ваши ответы будут сравниваться с максимально возможным для каждой идеологии, что даст вам процентное значение. Отвечайте честно! \
      Подобно 8values, эта викторина будет пытаться сопоставить вас с определенной правой идеологией. В настоящее время существует 36 идеологий, и их будет еще больше. Предложения очень приветствуются. \
-     Если у вас есть предложения или конструктивная критика, пожалуйста, откройте вопрос в Github, ссылка на который находится в правом верхнем углу этой страницы. напишите переводчику в телеграм, если у вас есть претензии к переводу. @ladomir37"
+     Если у вас есть предложения или конструктивная критика, пожалуйста, откройте вопрос в Github, ссылка на который находится в правом верхнем углу этой страницы. напишите переводчику в телеграм, если у вас есть претензии к переводу. @ladomir37",
 	
      "ja": "RightValuesとは、 <a href='https://leftvalues.github.io/' target='_blank'>LeftValues</a>から着想を得た、右派／右翼向け政治診断です。右のなかでも、あなたがどのような価値観をもっているのか診断します。 左寄りの方は正しい結果が得られませんので、LeftValuesをご利用ください。この診断では、表示される各文に対してどう考えるか、<b>「つよく同意する」</b>から<b>「まったく同意しない」</b>の範囲より答えていただきます。\
      選択に応じてスコアがつけられ、最後まで回答すると、それぞれの価値観について取り得る最大スコアを100％とした確率が表示されます。正直にお答えください！<br /><br /> \
      LeftValuesでは、発想元の<a href='https://8values-ja.github.io/index.html' target='_blank' rel='noopener noreferrer'>8values</a>や<a href='https://leftvalues.github.io/' target='_blank' rel='noopener noreferrer'>LeftValues</a> \
-     と似たように、あなたの回答に最も近い思想を診断します。現在36種類の思想が登録されており、順次拡大予定です。ご提案がありましたら、お気軽にご連絡ください。いずれの軸についても、この診断では100％をとる必要はありません。提案や建設的な批判がある場合は、このページの右上隅にリンクされているGithubで問題を開いてください (Translated by: Choux)"
+     と似たように、あなたの回答に最も近い思想を診断します。現在36種類の思想が登録されており、順次拡大予定です。ご提案がありましたら、お気軽にご連絡ください。いずれの軸についても、この診断では100％をとる必要はありません。提案や建設的な批判がある場合は、このページの右上隅にリンクされているGithubで問題を開いてください (Translated by: Choux)",
 	
      "ko": "Rightvalues는 <a href='https://leftvalues.github.io/' target='_blank' rel='noopener noreferrer'>LeftValues</a>의 대안으로 우익 스펙트럼 상에서 당신의 위치를 파악하기 위해 LeftValues를 바탕으로 만들어진 우익 설문입니다. 만약 당신이 우파가 아니라면, 이 설문은 분명 당신에게 적합하지 않습니다. \
      대신 LeftValues를 찾으세요. 각 문항을 살펴보고 <b>적극 동의</b>부터 <b>동의</b>, <b>중립/모르겠다</b>, <b>비동의</b>, <b>적극 비동의</b>까지의 답변중 하나를 선택하면, 각각의 답변이 당신의 점수에 조금씩 영향을 주게 될 것입니다. 설문 마지막에 당신의 답변에 기초한 점수가 각각의 가치(values)에 대해 가질 수 있는 최대값에 대한 백분율로 표시됩니다. 최대한 정직하게 대답하세요!<br /><br /> \
      질문은 총 <b><u>70</u></b> 문항이 있습니다. 8Values, LeftValues와 비슷하게, 이 설문은 특정한 우파 이념과 당신을 짝지으려 할 것입니다. 현재 36개의 가능한 이념이 있고, 미래에는 더 많은 이념들이 있을 수 있습니다. 이는 진행중인 작업이며, 의도한대로 작동하지 않을 수도 있습니다. 제안은 언제나 환영합니다. \
-     어떠한 범주에서든 100% 의 점수를 얻을 수 없다는 것을 인지해주세요. 이 설문의 목적은 당신 자신의 관점에 직면하는 것에 있습니다. 제안이나 건설적인 비판이 있는 경우 이 페이지의 오른쪽 상단에 링크된 Github에서 문제를 여십시오. (Translated by: Mizeery)"
+     어떠한 범주에서든 100% 의 점수를 얻을 수 없다는 것을 인지해주세요. 이 설문의 목적은 당신 자신의 관점에 직면하는 것에 있습니다. 제안이나 건설적인 비판이 있는 경우 이 페이지의 오른쪽 상단에 링크된 Github에서 문제를 여십시오. (Translated by: Mizeery)",
 	
      "cn": "RightValues是基于<a href='https://leftvalues.github.io/'>LeftValues</a>， 针对右派人士的一项测验，旨在确定您在右派人士上的定位。 如果您是左派人士，那么此测验显然不适合您，您应该寻找LeftValues。\
      系统将为您提供一个陈述，然后对陈述做出回答，从“ <b>非常同意</b>”到“<b>非常不同意</b>”, 每个答案都会稍微影响您的分数。 测验结束时，您的答案将与每个值的最大可能值进行比较，从而得出一个百分比。<br /><br /> 有<b><u>70</u></b>道题在此测试中。\
@@ -135,17 +135,17 @@ const i18n = {
 	
 "index-license": {
      "en": "8values is licensed under the MIT License, which permits 'without restriction' the rights to 'use, copy, modify, merge, publish, distribute, sublicense, and/or sell \
-     copies of the Software'. LeftValues is licensed under the same MIT License. RightValues is thus able to legally modify the original 8values without infringing on any copyright. RightValues is based on LeftValues and 8Values."
+     copies of the Software'. LeftValues is licensed under the same MIT License. RightValues is thus able to legally modify the original 8values without infringing on any copyright. RightValues is based on LeftValues and 8Values.",
 
      "ru": "8values находится под лицензией MIT License, которая разрешает «без ограничений» права на «использование, копирование, изменение, объединение, публикацию, распространение, сублицензирование и / или продажу \
      копий Программного обеспечения ». LeftValues находится под лицензией MIT License, которая разрешает« без ограничений »права на« использование, копирование, изменение, объединение, публикацию, распространение, сублицензию и / или продажу \
-     копии программного обеспечения ». RightValues, таким образом, может на законных основаниях изменять исходные 8values без нарушения каких-либо авторских прав. RightValues основывается на LeftValues и 8Values."
+     копии программного обеспечения ». RightValues, таким образом, может на законных основаниях изменять исходные 8values без нарушения каких-либо авторских прав. RightValues основывается на LeftValues и 8Values.",
 	
      "ja": "8valuesはMIT Licenseに基づいてライセンスされており、「無制限('without restriction')」に、「ソフトウェアの複製を使用、複写、変更、結合、掲載、頒布、サブライセンス、および/または販売する ('use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software')」権利が認められています。\
-     従って、LeftValuesは著作権を侵害することなく、適法的に8valuesへ変更を加えています。また、LeftValuesも同MIT Licenseを適用しています。RightValuesはLeftValues基にして制作されました。"
+     従って、LeftValuesは著作権を侵害することなく、適法的に8valuesへ変更を加えています。また、LeftValuesも同MIT Licenseを適用しています。RightValuesはLeftValues基にして制作されました。",
 	
      "cn": "8values 是采用 MIT 许可证授权的，该许可证“无限制地”授予“使用、复制、修改、合并、发布、分发、再授权和 / 或出售软件的副本”的权利。\
-     因此，LeftValues 能够在不侵犯任何版权的情况下合法地修改原始的 8values。同理，RightValues也能在不侵犯任何版权的情况下合法地修改 LeftValues，翻译者Songyon也能在不侵犯任何版权的情况下合法地翻译RightValues。"
+     因此，LeftValues 能够在不侵犯任何版权的情况下合法地修改原始的 8values。同理，RightValues也能在不侵犯任何版权的情况下合法地修改 LeftValues，翻译者Songyon也能在不侵犯任何版权的情况下合法地翻译RightValues。",
 
      "ko": "RightValues는 '사용, 복사, 수정, 병합, 게시, 배포, 하위 라이선스 및/또는 판매할 수 있는' 권한을 '제한 없이' 허용하는 MIT 라이선스에 따라 라이선스가 부여됩니다. \
      소프트웨어의 복사본'입니다. LeftValues는 '사용, 복사, 수정, 병합, 게시, 배포, 하위 라이선스 및/또는 판매 권한을 '제한 없이' 허용하는 MIT 라이선스에 따라 라이선스가 부여됩니다. \
@@ -181,7 +181,7 @@ const i18n = {
     Those with a higher Decelerate score tend to want a slower technological progress, and wants to conserve the current or past way of living. Those who score higher in Investment tend to think that acceleration brings many benefits to the market. \
 
     <b style='color:#27577A;'>Assimilation</b> <b>vs.</b> <b style='color:#C4A717;'>Multicultural</b> <br/> \
-    Those with a higher Assimilation score value a unified cultural identity. They tend to oppose multiculturalism, loose borders, and illegal immigration. Those with a higher Multicultural score think that multiple independent cultures should exist within society."
+    Those with a higher Assimilation score value a unified cultural identity. They tend to oppose multiculturalism, loose borders, and illegal immigration. Those with a higher Multicultural score think that multiple independent cultures should exist within society.",
 	
 	
     "ru": "В настоящее время существует шесть осей, каждая из которых имеет два противоположных мнения: <br> \
@@ -204,7 +204,7 @@ const i18n = {
     Те, у кого выше показатель замедления, склонны к более медленному техническому прогрессу и хотят сохранить текущий или прошлый образ жизни. Те, кто набирает более высокие баллы по 'ускорению', склонны думать, что ускорение приносит обществу много преимуществ. \
 
     <b style='color:#27577A;'>Ассимиляция</b> <b>vs.</b> <b style='color:#C4A717;'>Мультикультурный</b> <br/> \
-    Те, у кого более высокий балл ассимиляции, ценят единую культурную идентичность. Они склонны выступать против мультикультурализма, свободных границ и нелегальной иммиграции. Те, у кого более высокий показатель мультикультурности, думают, что в обществе должно существовать несколько независимых культур. "
+    Те, у кого более высокий балл ассимиляции, ценят единую культурную идентичность. Они склонны выступать против мультикультурализма, свободных границ и нелегальной иммиграции. Те, у кого более высокий показатель мультикультурности, думают, что в обществе должно существовать несколько независимых культур. ",
 
 	
    "ja": "診断は、次に示す6つの「主軸」に基づいて下されます。各主軸は、対極の価値観同士を端に配置してあります。<br> \
@@ -227,7 +227,7 @@ const i18n = {
     「減速」の方は科学や技術の発展を減速させ、現在や過去の文化を維持することに賛成します。「加速」派の方は、投資などを通して発展を促進させることを支持します。また、科学発展は市場へ利益があると考える場合も多くあります。 \
 
     <b style='color:#27577A;'>同化</b> <b>vs.</b> <b style='color:#C4A717;'>多文化</b> <br/> \
-    同化スコアが高い人は、統一された文化的アイデンティティを評価します。 彼らは多文化主義、ゆるい国境、不法移民に反対する傾向があります。 多文化スコアが高い人は、社会の中に複数の独立した文化が存在するべきだと考えています。 "	
+    同化スコアが高い人は、統一された文化的アイデンティティを評価します。 彼らは多文化主義、ゆるい国境、不法移民に反対する傾向があります。 多文化スコアが高い人は、社会の中に複数の独立した文化が存在するべきだと考えています。 ",	
 	
 	
    "cn": "当前一共有七个坐标轴，其各自代表了一对相反的意识形态。它们是：<br> \
@@ -250,7 +250,7 @@ const i18n = {
     减速得分较高的人倾向于希望技术进步较慢，并希望保持目前或过去的生活方式。激进得分较高的人倾向于认为加速会给市场带来很多好处。 \
 
     <b style='color:#27577A;'>同化</b> <b>vs.</b> <b style='color:#C4A717;'>多文化</b> <br/> \
-    同化分数较高的人重视统一的文化认同。 他们倾向于反对多元文化主义、宽松的边界和非法移民。 多元文化得分较高的人认为社会中应该存在多种独立的文化。"
+    同化分数较高的人重视统一的文化认同。 他们倾向于反对多元文化主义、宽松的边界和非法移民。 多元文化得分较高的人认为社会中应该存在多种独立的文化。",
 	
 	
     "ko": "현재 6개의 축이 있습니다. 각각의 축은 아래와 같은 상반되는 두 개의 가치를 갖게 됩니다.:<br> \
@@ -273,7 +273,7 @@ const i18n = {
     기술 발달 감속 점수가 높은 사람들은 기술 발전이 더딘 것을 선호하는 경향이 있으며 현재 또는 과거 생활 방식을 보존하고자 합니다. 기술 발달 가속 점수가 높은 사람들은 가속이 시장에 많은 이점을 가져다 준다고 생각하는 경향이 있습니다. \
 
     <b style='color:#27577A;'>동화</b> <b>vs.</b> <b style='color:#C4A717;'>다문화</b> <br/> \
-    동화 점수가 높은 사람들은 통일된 문화적 정체성을 중요하게 생각합니다. 그들은 다문화주의, 느슨한 국경, 불법 이민에 반대하는 경향이 있습니다. 다문화 점수가 높을수록 사회에는 여러 개의 독립적인 문화가 존재해야 한다고 생각합니다. "
+    동화 점수가 높은 사람들은 통일된 문화적 정체성을 중요하게 생각합니다. 그들은 다문화주의, 느슨한 국경, 불법 이민에 반대하는 경향이 있습니다. 다문화 점수가 높을수록 사회에는 여러 개의 독립적인 문화가 존재해야 한다고 생각합니다. ",
 },
 
 // quiz.html
